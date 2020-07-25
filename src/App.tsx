ee3352d488d70076b15adbf55735879c7313c249
GodDoesPlayDice <alik_okroyan@mail.ru>
2020-07-25 22:25:23 +0300

    lifting state up from table is working now (with useEffect)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from './components/table'
 
-function App() {
+function MainWrapper (props: any) {
+  let [state, setState] = React.useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {props.children}
+    </>
   );
 }
 
-export default App;
+export default function App() {
+
+  function onButtonClicked(data: any) {
+    console.log(data)
+  }
+
+  return (
+    <MainWrapper>
+     <Table 
+      liftStateUpFunc={onButtonClicked}
+     />
+    </MainWrapper>
+  );
+}
